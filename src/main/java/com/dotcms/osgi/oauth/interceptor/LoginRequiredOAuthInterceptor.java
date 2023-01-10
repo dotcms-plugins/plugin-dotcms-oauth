741129364633142d2efbe1af508ae340100c331a
jdotcms <jonathan.sanchez@dotcms.com>
2023-01-10 12:40:15 -0600

    Adding fix for the redirect to idp

--- src/main/java/com/dotcms/osgi/oauth/interceptor/LoginRequiredOAuthInterceptor.java
+++ src/main/java/com/dotcms/osgi/oauth/interceptor/LoginRequiredOAuthInterceptor.java
@@ -7,10 +7,13 @@ import static com.dotcms.osgi.oauth.util.Constants.OAUTH_REDIRECT;
 import static com.dotcms.osgi.oauth.util.Constants.OAUTH_SERVICE;
 import static com.dotcms.osgi.oauth.util.Constants.REFERRER;
 import java.io.IOException;
+import java.io.StringWriter;
 import java.util.List;
 import java.util.Optional;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.dotmarketing.util.UtilMethods;
 import org.scribe.builder.ServiceBuilder;
 import org.scribe.builder.api.DefaultApi20;
 import org.scribe.model.Token;
@@ -184,19 +187,30 @@ public class LoginRequiredOAuthInterceptor implements WebInterceptor {
         Logger.info(this.getClass().getName(), "Redirecting for authentication to: " + authorizationUrl);
 
         System.err.println("Initial Session Id: " + request.getSession().getId());
-        
-        
-        
-        Try.run(() -> {
-            response.getWriter().println("<html><head><meta http-equiv=\"refresh\" content=\"0;URL='" + authorizationUrl + "'\" /></head><body></body></html>");
-            response.getWriter().close();
-        }
-        );
 
-        
-        //response.sendRedirect(authorizationUrl);
+        sendRedirectHTML(response, authorizationUrl);
     }
 
+    final static String redirectTemplate =
+            new StringWriter()
+                    .append("<html>")
+                    .append("<head>")
+                    .append("<meta http-equiv=\"refresh\" content=\"0;URL='REDIRECT_ME'\"/>")
+                    .append("<style>p {font-family: Arial;font-size: 16px;color: #666;margin: 50px;text-align:center;opacity: 1;animation: fadeIn ease 5s;animation-iteration-count: 0;-webkit-animation: fadeIn ease 5s;}@keyframes fadeIn {0% {opacity:0;}100% {opacity:1;}}@-moz-keyframes fadeIn {0% {opacity:0;}100% {opacity:1;}}@-webkit-keyframes fadeIn {0% {opacity:0;}100% {opacity:1;}}@-o-keyframes fadeIn {0% {opacity:0;}100% {opacity:1;}@-ms-keyframes fadeIn {0% {opacity:0;}100% {opacity:1;}}</style>")
+                    .append("</head>")
+                    .append("<body><p>If your browser does not refresh, click <a href=\"REDIRECT_ME\">Here</a>.</p></body>")
+                    .append("</html>")
+                    .toString();
 
+    public static void sendRedirectHTML(HttpServletResponse response, final String redirectUrl) {
+
+        final String finalTemplate = UtilMethods.replace(redirectTemplate,"REDIRECT_ME", redirectUrl);
+
+        response.setContentType("text/html");
+        Try.run(() -> {
+            response.getWriter().write(finalTemplate);
+            response.getWriter().flush();
+        }).onFailure(e->Logger.warn(LoginRequiredOAuthInterceptor.class,"Unable to redirect to :" + redirectUrl+ " cause:"+e.getMessage()));
+    }
 
 } // BackEndLoginRequiredOAuthInterceptor.
